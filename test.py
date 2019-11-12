76533d2893e6cb0ea6be98c8dc595cfea88e2b72
Jeremy Griffin <jeremy.griffin@nccgroup.com>
2019-11-12 15:06:55 +0000

    test

---
+++ test.py
@@ -0,0 +1,33 @@
+print("This is going to be some bad code for sonarqube to test.")
+
+
+# First SonarQube test for Spaghetti Code.
+data = input("Please enter some data: ")
+data2 = input("Please enter some more data: ")
+
+if data == "orange":
+
+    if data == "orange" and data2 == "red":
+
+        print("Data 1 is {data} and Data 2 is {data2}".format(data=data,
+            data2=data2))
+
+    if data == "orange" and data2 == "yellow":
+
+        print("Data1 is {data} and Data 2 is {data2}".format(data=data, data2=data2))
+
+    if data == data2:
+
+        print("Data 1 is {data} and Data 2 is {data2}".format(data=data,
+            data2=data2))
+
+    if data == "yellow" and data2 == "red":
+
+        print("Data 1 is {data} and Data 2 is {data2}".format(data=data,
+            data2=data))
+
+    if data == "purple" and data2 == "green":
+
+        print("Data 1 is {data} and Data 2 is {data2}".format(data=data,
+            data2=data2))
+
