1dfa0bf626a08f7647d39e17cb5def93aadad9e5
Jeremy Griffin <jeremy.griffin@nccgroup.com>
2019-11-13 09:27:32 +0000

    Added call to dodgy functioN

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 import subprocess
 
-def transcode file (request, filename):
+def transcode_file (request, filename):
     command = 'ffmpeg -i "{source}" output file.mpg'.format(source=filename)
     subprocess.call(command, shell=True)
 
 print("Hello World")
 
+file = input("Enter filename: ")
+transcode_file("Request", file)
 
